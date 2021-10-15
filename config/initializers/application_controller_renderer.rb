2d08ead9b4756b3fc9d19994d0134be3cb9d7b1e
TaishiNishimura <taishi19880324@gmail.com>
2021-10-15 14:08:11 +0000

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0e0ea546da1fd27855d45524ae65eb03005eaf01a10de143ffa491c266a12123963a47e84afb953d04723af7f8264f16fee2013883559c30838dd986fd53952a'
---
+++ config/routes.rb
@@ -0,0 +1,38 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customers, controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations',
+  }
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    get 'unsubscribe' => 'customers#unsubscribe'
+    patch 'withdraw' => 'customers#withdraw'
+    delete 'destroy_all' => 'cart_items#destroy_all'
+    post 'confirm' => 'orders#confirm'
+    get 'confirm' => 'orders#error'
+    get 'thanks' => 'orders#thanks'
+    resources :customers, only: [:show, :edit, :update]
+    resources :orders, only: [:new, :create, :index, :show]
+    resources :addresses, except: [:new, :show]
+    resources :items, only: [:index, :show] do
+      resources :cart_items, only: [:create, :update, :destroy]
+    end
+    resources :cart_items, only: [:index]
+  end
+
+
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+  }
+  namespace :admin do
+    get 'top' => 'homes#top', as: 'top'
+    resources :items, except: [:destroy]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:index, :show, :update] do
+      resources :order_details, only: [:update]
+    end
+  end
+end
