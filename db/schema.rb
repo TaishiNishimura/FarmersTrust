d26c71a71886bbe94657628de3e653dceec77c5e
TaishiNishimura <taishi19880324@gmail.com>
2021-10-19 14:57:29 +0000

    お問合せ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_18_094643) do
+ActiveRecord::Schema.define(version: 2021_10_19_140753) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2021_10_18_094643) do
     t.string "name", null: false
     t.string "email", null: false
     t.string "phone_number", null: false
-    t.integer "subject", default: 0, null: false
+    t.string "subject", null: false
     t.text "message", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
